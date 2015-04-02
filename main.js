58c4eaa90aa5557409294fe7cfc84f84fc57c250
EvgeniVasilev <evgenivasilev1209@abv.bg>
2015-04-02 20:33:34 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+
+TweenMax.to("#calmHearth",0.5, {scale:0.8, repeat: 1000, yoyo: true});
+
+TweenMax.to("#angryHearth", 0.3, {scale:0.8, repeat: 1000, yoyo: true});
+
+TweenMax.to("#scaredHearth", 0.2, {scale:0.8, repeat: 1000, yoyo: true});
